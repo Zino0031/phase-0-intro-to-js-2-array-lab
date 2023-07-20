32388c1d61d45083e961940ad18ea84dd66a844e
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-07-20 20:56:54 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function  destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function  appendCat(name) {
+
+    const newcat = cats.concat (name);
+
+    return newcat;
+
+}
+
+function prependCat(name){
+    const neewcat = [name, ...cats];
+    return neewcat;
+}
+
+function  removeLastCat(){
+    const newcat = cats.slice(0,cats.length -1);
+    return newcat;
+
+}
+
+function removeFirstCat(){
+    const newcat = cats.slice(1);
+    return newcat;
+}
